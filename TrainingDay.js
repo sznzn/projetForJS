48acf81bd229929c9429a75347e7c0331bd5af70
sznzn <sznzn@github.com>
2023-01-12 20:10:23 +0100

    TrainingDay

---
+++ TrainingDay.js
@@ -0,0 +1,55 @@
+const getRandEvent = () => {
+    const random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+        return 'Marathon';
+    } else if (random === 1) {
+        return 'Triathlon';
+    } else if (random === 2) {
+        return 'Pentathlon';
+    }
+};
+
+
+const getTrainingDays = event => {
+    let days;
+    if (event === 'Marathon') {
+        days = 50;
+    } else if (event === 'Triathlon') {
+        days = 100;
+    } else if (event === 'Pentathlon') {
+        days = 200;
+    }
+    return days;
+};
+    
+
+console.log(event1);
+
+const logEvent = (name, event) => {
+    console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+    console.log(`${name}'s time to train is: ${days} days`);
+};
+
+
+const name1 = 'Nala';
+const event1 = getRandEvent();
+const days1 = getTrainingDays(event1);
+
+logEvent(name1, event1);
+logTime(name1, days1);
+
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+   
+logEvent(name2, event2);
+logTime(name2, days2);
+
+
+  
+  
+  
